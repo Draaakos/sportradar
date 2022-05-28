cb142c57698b25a0dee1917d4e83e1efcb6d2c21
Drakos <orlando.andaur.c@gmail.com>
2022-05-28 00:24:18 -0300

    adde team calculate

--- scripts/sportradar.py
+++ scripts/sportradar.py
@@ -1,11 +1,42 @@
 import requests
+import datetime
 from .enum import Sport
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    IMPORTANT = '\33[42m'
+
 
 class Sportradar:
     def __init__(self, filename, name):
         self.name = name
         self.base_url = 'https://stats.fn.sportradar.com'
         self.sport_selected = None
+
+
+    def fetch_matches_by_season(self, season_id):
+        url = f'sportradar/en/America:Montevideo/gismo/stats_season_fixtures2/{season_id}/1'
+        try:
+            response = requests.get(f'{self.base_url}/{url}').json()
+            return {
+                "error": False,
+                "response": response,
+            }
+        except:
+            return {
+                "error": True,
+                "response": {},
+                "reason": "response"
+            }
 
 
     def fetch_team_list_by_season(self, season_id):
@@ -265,30 +296,113 @@ if __name__ == '__main__':
         return reference_selected
 
 
-    clear_console()
+
+
+
+    # FETCH TEAM LIST AND SEASON INFORMATION
+    team_list = {}
+    team_list_table_data = []
     response_team_list = sportradar.fetch_team_list_by_season(season_id)
     if response_team_list['error'] == False:
         team_list_table_data = response_team_list['response']['doc'][0]['data']['tables'][0]['tablerows']
 
-        team_list = []
         for i, team_row in enumerate(team_list_table_data):
-            team_list.append({
-                'name': team_row['team']['name'],
+            team_list[team_row['team']['_id']] = {
+                'name': team_row['team']['name'], 
                 'reference': i
-            })
+            }
+
+    match_list_by_season = sportradar.fetch_matches_by_season(season_id)
+    if match_list_by_season['error'] == False:
+        matches = match_list_by_season['response']['doc'][0]['data']['matches']
+
+        now = datetime.datetime.now()
+        current_round = None
+        next_matches = []
+        for match in matches:
+            match_home_result = match['result']['home']
+            match_away_result = match['result']['away']
+            
+            match_time_splitted = match['time']['date'].split('/')
+            match_time_day = int(match_time_splitted[0])
+            match_time_month = int(match_time_splitted[1])
+            match_time_year = int(f'20{match_time_splitted[2]}')
+            match_time = datetime.date(match_time_year, match_time_month, match_time_day)
+
+            if (match['result']['home'] == None and match['result']['away'] == None) and (match_time.year >= now.year and match_time.month >= now.month and match_time.day >= now.day):
+                if current_round == None:
+                    current_round = match['round']
+
+                if match['round'] == current_round:
+                    next_matches.append(match)
+                    
         
-        team_selected_home = select_menu_team_list(team_list, 'HOME TEAM')
-        team_selected_away = select_menu_team_list(team_list, 'AWAY TEAM')
-
-        home_team_data = team_list_table_data[team_selected_home['reference']]
-        away_team_data = team_list_table_data[team_selected_away['reference']]
-
         clear_console()
-        print(f'{home_team_data["team"]["name"]} VS {away_team_data["team"]["name"]}')
-        expected_goals = round(calculate_over_under_goals(home_team_data, away_team_data), 2)
+        print(f'THE NEXT MATCHES FOR ROUND {current_round} ARE:')
+        print('***********************************************')
+        for match in next_matches:
+            match_time = match['time']['date']
+            home_team_data_position = team_list[match['teams']['home']['_id']]['reference']
+            home_team_data = team_list_table_data[home_team_data_position]
 
-        if expected_goals >= 3:
-            print(f'the spected goals for this game is {round(expected_goals, 2)}')
-            print('recommended!!')
-        else:
-            print(f'is not recommended this game with expected goals: {expected_goals}')
+            away_team_data_position = team_list[match['teams']['away']['_id']]['reference']
+            away_team_data = team_list_table_data[away_team_data_position]
+
+            home_team_name = match['teams']['home']['name']
+            away_team_name = match['teams']['away']['name']
+
+            try:
+                expected_goals = round(calculate_over_under_goals(home_team_data, away_team_data), 1)
+                print(f'NAME: {home_team_name} VS {away_team_name}')
+                print(f'DATE: {match_time}')
+                print(f'SPECTED_GOALS: {expected_goals}')
+
+                if expected_goals >= 3:
+                    if expected_goals >= 4:
+                        print(bcolors.IMPORTANT + "RECOMMENDED +2 OR +2.5 GOALS IN THE GAME" + bcolors.ENDC)
+                    else:
+                        print(bcolors.OKGREEN + "RECOMMENDED +2 GOALS IN THE GAME" + bcolors.ENDC)
+                elif expected_goals <= 1.5:
+                    if expected_goals <= 1:
+                        print(bcolors.IMPORTANT + "RECOMMENDED -2.5 OR -3 GOALS IN THE GAME" + bcolors.ENDC)
+                    else:
+                        print(bcolors.OKGREEN + "RECOMMENDED -3 GOALS IN THE GAME" + bcolors.ENDC)
+                else:
+                    print(bcolors.FAIL + "NOT RECOMMENDED" + bcolors.ENDC)
+                print('***********************************************')
+            except:
+                print('error')
+            
+            
+
+
+
+
+
+    # clear_console()
+    # response_team_list = sportradar.fetch_team_list_by_season(season_id)
+    # if response_team_list['error'] == False:
+    #     team_list_table_data = response_team_list['response']['doc'][0]['data']['tables'][0]['tablerows']
+
+    #     team_list = []
+    #     for i, team_row in enumerate(team_list_table_data):
+    #         team_list.append({
+    #             'name': team_row['team']['name'],
+    #             'reference': i
+    #         })
+        
+    #     team_selected_home = select_menu_team_list(team_list, 'HOME TEAM')
+    #     team_selected_away = select_menu_team_list(team_list, 'AWAY TEAM')
+
+    #     home_team_data = team_list_table_data[team_selected_home['reference']]
+    #     away_team_data = team_list_table_data[team_selected_away['reference']]
+
+    #     clear_console()
+    #     print(f'{home_team_data["team"]["name"]} VS {away_team_data["team"]["name"]}')
+    #     expected_goals = round(calculate_over_under_goals(home_team_data, away_team_data), 2)
+
+    #     if expected_goals >= 3:
+    #         print(f'the spected goals for this game is {round(expected_goals, 2)}')
+    #         print('recommended!!')
+    #     else:
+    #         print(f'is not recommended this game with expected goals: {expected_goals}')
